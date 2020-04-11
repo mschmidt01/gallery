2d157fec7a6d63a32fdb85726a9dcd3c4f06d03e
Larissa Schneider <lmek.schneider@gmail.com>
2020-04-11 14:50:06 +0200

    Lokale EInbindung von Schriften/CDN

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -7,8 +7,6 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>BoS Gallery</title>
     <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/monokai.min.css">
 </head>
 <body>
 <div id="app">
