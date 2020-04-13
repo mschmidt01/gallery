506bdd78037c9aab357b448775759a720d8a9d2d
swlz <mobbit@gmx.de>
2020-04-13 16:21:48 +0200

    updated gallery to master

--- resources/js/components/content/Gallery.vue
+++ resources/js/components/content/Gallery.vue
@@ -1,116 +1,298 @@
 <template>
-  <div class="container-fluid">
-    <h2>Gallery</h2>
-    <div>
-      <p>Themen</p>
-      <ul>
-        <li v-for="topic in filters.topics" :key="topic.name">
-          <input
-            type="checkbox"
-            v-bind:value="topic"
-            :id="topic"
-            v-model="topicfilter"
-            v-on:change="filterPictures()"
-          />
-          <label :for="topic">{{topic}}</label>
-          <br />
-        </li>
-      </ul>
-    </div>
-    <p>Filter:</p>
-    <div>
-      <p>Module</p>
-      <ul>
-        <li v-for="filtername in filters.modules" :key="filtername">
-          <input
-            type="checkbox"
-            v-bind:value="filtername"
-            :id="filtername"
-            v-model="modulfilter"
-            v-on:change="filterPictures()"
-          />
-          <label :for="filtername">{{filtername}}</label>
-          <br />
-        </li>
-      </ul>
-    </div>
-    <div>
-      <p>Semester</p>
-      <ul>
-        <li v-for="filtername in filters.classes" :key="filtername">
-          <input
-            type="checkbox"
-            v-bind:value="filtername"
-            :id="filtername"
-            v-model="classfilter"
-            v-on:change="filterPictures()"
-          />
-          <label :for="filtername">{{filtername}}</label>
-          <br />
-        </li>
-      </ul>
-    </div>
-    <div>
-      <p>Sterne</p>
-      <ul>
-        <li v-for="index in 5" :key="index">
-          <input
-            type="checkbox"
-            v-bind:value="index"
-            v-model="starfilter"
-            v-on:change="filterPictures()"
-          />
-          <label>{{index}}</label>
-          <br />
-        </li>
-      </ul>
-    </div>
-    <div>
-      <p>Sortieren nach:</p>
-      <ul>
-        <a v-on:click="sort($event,sortOptions[0])" href="#">
-          <li>alphabetisch</li>
-        </a>
-        <a v-on:click="sort($event,sortOptions[1])" href="#">
-          <li>Datum</li>
-        </a>
-      </ul>
-    </div>
-    <div class="row" v-for="image in filtered" :key="image.Filename">
-      <img height="42" width="42" :src="'/img/gallery/' +  image.Path + '/'+ image.Filename " />
-      <!-- Debugging{{image.Gallery}},{{image.THMModule}},{{image.Class}},{{image.Filename}},{{image.Timestamp}},{{image.Votes}},{{image.Rating}},Bewertung:{{image.Rating/image.Votes}}-->
-      Kommentare:
-      <div v-for="comment in image.comments" :key="comment.Text">
-        <p>{{comment.Text}}</p>
-        <br />
+  <div v-on:scroll="alert()">
+    <h1 class="title">Gallery</h1>
+    <section>
+      <button
+        type="button"
+        class="w-100 btn filter-button dropdown-toggle"
+        data-toggle="collapse"
+        data-target="#filter-panel"
+        aria-expanded="false"
+      >Filteroptionen</button>
+      <div id="filter-panel" class="collapse w-100 container p-3">
+        <div class="row">
+          <div class="col-md-2">
+            <strong>Topic</strong>
+          </div>
+          <div class="col-md-10">
+            <div class="form-check form-check-inline" v-for="topic in filters.topics" :key="topic">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-bind:value="topic"
+                :id="topic"
+                v-model="topicfilter"
+                v-on:change="filterPictures()"
+              />
+              <label class="form-check-label" :for="topic">{{topic}}</label>
+            </div>
+          </div>
+        </div>
+        <div class="row pt-3">
+          <div class="col-md-2">
+            <strong>Modul</strong>
+          </div>
+          <div class="col-md-10">
+            <div
+              class="form-check form-check-inline"
+              v-for="filtername in filters.modules"
+              :key="filtername"
+            >
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-bind:value="filtername"
+                :id="filtername"
+                v-model="modulfilter"
+                v-on:change="filterPictures()"
+              />
+              <label class="form-check-label" :for="filtername">{{filtername}}</label>
+            </div>
+          </div>
+        </div>
+        <div class="row pt-3">
+          <div class="col-md-2">
+            <strong>Semester</strong>
+          </div>
+          <div class="col-md-10">
+            <div
+              class="form-check form-check-inline"
+              v-for="semester in filters.classes"
+              :key="semester"
+            >
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-bind:value="semester"
+                :id="semester"
+                v-model="classfilter"
+                v-on:change="filterPictures()"
+              />
+              <label class="form-check-label" :for="semester">{{semester}}</label>
+            </div>
+          </div>
+        </div>
+        <div class="row pt-3">
+          <div class="col-md-2">
+            <strong>Sterne</strong>
+          </div>
+          <div class="col-md-10">
+            <div class="form-check form-check-inline" v-for="index in 5" :key="index">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-bind:value="index"
+                :id="index"
+                v-model="starfilter"
+                v-on:change="filterPictures()"
+              />
+              <label class="form-check-label" :for="index">
+                {{index}}
+                <span class="fa fa-star"></span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="row pt-3">
+          <div class="col-md-2">
+            <span class="inline-block align-center">
+              <strong>Sortierung</strong>
+            </span>
+          </div>
+          <div class="col-md-10">
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                v-on:click="sort($event,sortOptions[0])"
+              >Alphabet</button>
+              <button
+                type="button"
+                class="btn btn-secondary"
+                v-on:click="sort($event,sortOptions[1])"
+              >Datum</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <star-rating
-        :round-start-rating="false"
-        :rating="image.Rating/image.Votes"
-        :show-rating="false"
-        @rating-selected="setRating(image.PID,$event)"
-      ></star-rating>
-      <input type="text" :id="'comment_' + image.PID" />
-      <div class="contact-field">
-        Bitte Feld leer lassen
-        <input v-model="contact" type="text" name="contact" />
+      <div class="mt-5">
+        <div
+          v-if="selectedImage"
+          class="modal fade bd-example-modal-lg"
+          id="imageDialog"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="imageDialog"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">{{selectedImage.Filename}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="row">
+                  <div class="col-12 col-lg-6">
+                    <img
+                      width="100%"
+                      :src="'/img/gallery/' +  selectedImage.Path + '/'+ selectedImage.Filename "
+                      id="exampleModalLabel"
+                    />
+                  </div>
+                  <div class="col-12 col-lg-6 mt-3 mt-lg-0">
+                    <h5>Bewertungen</h5>
+                    <star-rating
+                      class="mb-2 stars"
+                      :round-start-rating="false"
+                      :rating="selectedImage.Rating/selectedImage.Votes"
+                      v-bind:star-size="20"
+                      :show-rating="false"
+                      active-color="#343A40"
+                      inactive-color="#fff"
+                      border-color="#343A40"
+                      :border-width="2"
+                      :rounded-corners="true"
+                    ></star-rating>
+                    <div v-if="selectedImage.comments.length>0" class="scroll-container">
+                      <div v-for="comment in selectedImage.comments" :key="comment.Text">
+                        <small>{{comment.Text}}</small>
+                        <br />
+                      </div>
+                    </div>
+                    <hr />
+                    <h5>Eigene Bewertung abgeben</h5>
+                    <form>
+                      <star-rating
+                        class="mb-1 stars"
+                        :round-start-rating="false"
+                        :rating="0"
+                        v-bind:star-size="20"
+                        :show-rating="false"
+                        active-color="#343A40"
+                        inactive-color="#fff"
+                        border-color="#343A40"
+                        :border-width="2"
+                        :rounded-corners="true"
+                        @rating-selected="getRating"
+                      ></star-rating>
+                      <div class="form-group">
+                        <label
+                          for="'comment_' + selectedImage.PID"
+                          class="col-form-label"
+                        >Kommentar:</label>
+                        <textarea class="form-control" :id="'comment_' + selectedImage.PID"></textarea>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-sec" data-dismiss="modal">Schließen</button>
+                <button
+                  type="button"
+                  class="btn btn-blue"
+                  v-on:click="commentPicture(selectedImage.PID); setRating(selectedImage.PID)"
+                >Bewerten</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div
+            class="col-lg-1 col-md-2 col-3 py-2"
+            v-for="(image, index) in filtered"
+            v-if="index<showPictureAmount"
+            :key="index"
+          >
+            <a data-toggle="modal">
+              <img
+                loading="lazy"
+                class="gallery-image"
+                :src="'/img/gallery/' +  image.Path + '/'+ image.Filename.split('.')[0] + '-thumb.png'"
+                alt="Some Image"
+                @click="openImageDialog(image)"
+              />
+              <!--<img loading="auto" class="gallery-image rounded" :src="'/img/gallery/' +  image.Path + '/'+ image.Filename "/>-->
+            </a>
+          </div>
+        </div>
+
+        <!--<div class="row" v-for="(image, index) in filtered">
+                    <img height="42" width="42" :src="'/img/gallery/' +  image.Path + '/'+ image.Filename "/>
+                    Kommentare:
+                    <div v-for="comment in image.comments">
+                        <p>{{comment.Text}}</p>
+                        <br>
+                    </div>
+                    <star-rating :round-start-rating="false" :rating="image.Rating/image.Votes" :show-rating="false"
+                                 @rating-selected="setRating(image.PID,$event)"></star-rating>
+                    <input type="text" :id="'comment_' + image.PID">
+                    <button v-on:click="commentPicture(image.PID)">Kommentieren</button>
+        </div>-->
       </div>
-      <div v-show="!hideInput" class="text-field">
-        Bitte Feld nicht ändern
-        <input v-model="email" type="text" name="email" />
-      </div>
-      <button v-on:click="commentPicture(image.PID)">Kommentieren</button>
-    </div>
+    </section>
   </div>
 </template>
+
+<style scoped>
+input[type="checkbox"] {
+  display: none;
+}
+input[type="checkbox"] + label::before {
+  width: 15px;
+  height: 15px;
+  border: 2px solid blue;
+  background-color: #fff;
+  display: block;
+  content: "";
+  float: left;
+  margin-right: 5px;
+  margin-top: 5px;
+  cursor: pointer;
+}
+input[type="checkbox"]:checked + label::before {
+  box-shadow: inset 0px 0px 0px 2px #fff;
+  background-color: blue;
+}
+#filter-panel {
+  background-color: white;
+  border: 1px solid #343a40;
+}
+.filter-button {
+  background-color: #343a40;
+  color: white;
+  border-radius: 0px;
+  height: 50px;
+}
+.filter-button:focus {
+  box-shadow: none;
+}
+.fa-star {
+  color: #343a40;
+}
+.btn-secondary:active {
+  background-color: #343a40;
+}
+.btn-secondary:hover {
+  background-color: #343a40;
+}
+.gallery-image {
+  width: 100%;
+}
+.scroll-container {
+  height: 100px;
+  overflow-y: scroll;
+}
+</style>
+
 <script>
 import axios from "axios";
 export default {
   data() {
-    const cookieArray = document.cookie.replace(" ", "").split(";");
-    const recaptchaConsent = cookieArray.includes("recaptcha=true")
-      ? true
-      : false;
     return {
       loading: false,
       error: null,
@@ -127,13 +309,19 @@ export default {
       starfilter: [],
       sortOptions: ["alphabetical", "date"],
       filterType: ["modules", "classes"],
-      recaptchaConsent,
-      hideInput: true,
-      contact: "",
-      email: "test@email.com"
+      showPictureAmount: 0,
+      selectedImage: null,
+      rating: 0
     };
   },
   created() {
+    this.showPictureAmount = ((window.innerHeight - 280) / 66) * 12;
+    window.addEventListener("scroll", () => {
+      let offset =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      this.showPictureAmount = ((window.innerHeight + offset) * 12) / 66;
+      //console.log(this.showPictureAmount);
+    });
     this.topic = this.$route.params.name;
     if (typeof this.$route.params.name !== "undefined") {
       this.topicfilter[0] = this.topic;
@@ -144,48 +332,61 @@ export default {
     this.fetchData();
   },
   methods: {
-    setRating: function(id, rating) {
-      this.rateAndValidate(id, rating);
+    openImageDialog: function(image) {
+      console.log(image);
+      this.selectedImage = image;
+      $("#imageDialog").modal("show");
+    },
+    setRating: function(id) {
+      this.rateAndValidate(id, this.rating);
+    },
+    getRating: function(rating) {
+      this.rating = rating;
     },
     commentPicture: function(id) {
       let text = document.getElementById("comment_" + id).value;
       this.commentAndValidate(id, text);
     },
     async rateAndValidate(id, rating) {
-      let token = "";
-      if (this.recaptchaConsent) {
-        // (optional) Wait until recaptcha has been loaded.
+      // (optional) Wait until recaptcha has been loaded.
+      try {
         await this.$recaptchaLoaded();
-        // Execute reCAPTCHA with action "login".
-        token = await this.$recaptcha("starrating");
+      } catch (e) {
+        console.log(e);
       }
+      // Execute reCAPTCHA with action "login".
+      const token = await this.$recaptcha("starrating");
       axios.post("/api/pictures/vote", {
         token: token,
         imagid: id,
-        rating: rating,
-        email: this.email,
-        contact: this.contact
+        rating: rating
       });
       this.fetchData();
       // Do stuff with the received token.
     },
     async commentAndValidate(id, text) {
-      let token = "";
-      if (this.recaptchaConsent) {
-        // (optional) Wait until recaptcha has been loaded.
+      // (optional) Wait until recaptcha has been loaded.
+      try {
+        console.log("captcah");
         await this.$recaptchaLoaded();
+        console.log("nach captcah", x);
         // Execute reCAPTCHA with action "login".
-        token = await this.$recaptcha("comment");
+        const token = await this.$recaptcha("comment");
+        console.log("nachn captcah 2");
+        axios.post("/api/pictures/comment", {
+          token: token,
+          imagid: id,
+          text: text
+            .replace(/"/g, '\\"')
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+        });
+        console.log("nachn post");
+        this.fetchData();
+        // Do stuff with the received token.
+      } catch (e) {
+        console.log("das ging nicht", e);
       }
-      axios.post("/api/pictures/comment", {
-        token: token,
-        imagid: id,
-        text: text,
-        email: this.email,
-        contact: this.contact
-      });
-      this.fetchData();
-      // Do stuff with the received token.
     },
     fetchData() {
       this.loading = true;
